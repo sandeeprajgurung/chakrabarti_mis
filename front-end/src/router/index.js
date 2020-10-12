9defd0e0bd338eb014d8fd07ac1a0837e3e916f0
sandeep <sandeep@proshore.eu>
2020-10-12 11:19:24 +0545

    results implementation design

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Dashboard from '@/views/Dashboard'
 import Students from '@/views/students/Students'
+import Results from '@/views/results/Results'
 
 Vue.use(VueRouter)
 
@@ -14,6 +15,11 @@ const routes = [{
         path: '/students',
         name: 'Students',
         component: Students
+    },
+    {
+        path: '/results',
+        name: 'Results',
+        component: Results
     }
 ]
 
---
+++ front-end/src/views/results/Results.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <v-card>
+            <v-container fluid>
+                <v-row>
+                    <v-col md="6">
+                        <v-autocomplete v-model="values" :items="items" outlined label="Search students">
+                        </v-autocomplete>
+                    </v-col>
+                    <v-col md="6">
+                        <v-select :items="items" label="Select programs" outlined></v-select>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
+    </section>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            items: ['foo', 'bar', 'fizz', 'buzz'],
+            values: ['foo', 'bar'],
+            value: null,
+        }),
+    }
+</script>
