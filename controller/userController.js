6afefa785e0428b253a93710cef7220385697cf1
Arun Rai <arunrai@Aruns-MacBook-Pro.local>
2020-10-22 16:53:24 +0545

    push

--- controller/userController.js
+++ controller/userController.js
@@ -8,17 +8,19 @@ const User = db.USER;
 
 exports.Create = (req, res) => {
 
-    User.findAll({where: {
+    User.findAll({
+        where: {
             username: req.body.username
-         }})
-        .then(user =>{
-            if(user.length >= 1){
+        }
+    })
+        .then(user => {
+            if (user.length >= 1) {
                 return res.status(409).json({
                     message: "Username already exist"
                 })
             } else {
                 bcrypt.hash(req.body.password, 10, (err, hash) => {
-                    if(err){
+                    if (err) {
                         return res.send(500).json({
                             error: err
                         })
@@ -26,17 +28,17 @@ exports.Create = (req, res) => {
                         const user = {
                             username: req.body.username,
                             password: hash
-                          };
-            
-                          User.create(user)
+                        };
+
+                        User.create(user)
                             .then(data => {
-                            res.send(data);
+                                res.send(data);
                             })
                             .catch(err => {
-                            res.status(500).send({
-                                message:
-                                err.message || "Some error occurred while creating the User."
-                            });
+                                res.status(500).send({
+                                    message:
+                                        err.message || "Some error occurred while creating the User."
+                                });
                             });
                     }
                 })
@@ -47,46 +49,47 @@ exports.Create = (req, res) => {
 
 exports.login = (req, res) => {
 
-    User.findAll({where: {
-        username: req.body.username
-     }})
-     .then(user => {
-        if(user.length < 1){
-            return res.status(401).json({
-                message: "Auth failed"
-            })
-        }
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-            console.log(req.body.username)
-            // if(err){
-            //     return res.status(401).json({
-            //         message: "Auth failed"
-            //     })
-            // }
-            // if(result){
+    User.findAll({
+        where: {
+            username: req.body.username
+     }
+    })
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({
+                    message: "Auth failed"
+                })
+            }
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                // if(err){
+                //     return res.status(401).json({
+                //         message: "Auth failed"
+                //     })
+                // }
+                // if(result){
                 const token = jwt.sign({
                     username: user[0].username,
-                    id : user[0].id
+                    id: user[0].id
                 },
-                'secret',
-                {
-                    expiresIn: "1h"
-                }
+                    'secret',
+                    {
+                        expiresIn: "1h"
+                    }
                 );
                 return res.status(401).json({
                     message: "Auth Successfull",
                     token: "Bearer " + token
                 })
-            // }
-            // return res.status(401).json({
-            //     message: "Auth failed"
-            // })
+                // }
+                // return res.status(401).json({
+                //     message: "Auth failed"
+                // })
+            })
         })
-     })
-     .catch(err => {
-        res.status(500).send({
-            message:
-            err.message || "Some error occurred while creating the User."
-        });
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while login."
+            });
         });
 }
